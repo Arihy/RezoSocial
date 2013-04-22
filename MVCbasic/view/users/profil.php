280ea0923ac2bce1df132bfda803c622f008490e
Arihy <ynallem@gmail.com>
2013-04-22 16:48:56 +0200

    ajout navigation profil

--- MVCbasic/view/users/profil.php
+++ MVCbasic/view/users/profil.php
@@ -3,18 +3,44 @@
     <div class="span10">
       <!--Body content-->
       <div id="writeMsg">
-		<h4>Exprimez-vous</h4>
-		<form action="<?php echo RACINE.DS.'messages'.DS.'saveMessage' ?>" method="POST">
-			<input type="textarea" name="content" id="msgWall"><span class="add-on"><i class="icon-message"></i></span>
-			<button class="btn btn-primary" id="btn-send">send</button>
-		</form>
-		</div>
-      <article id="ressources">
-		<h4>Messages</h4>
-		<p id="msg-content">
-		</p>
+  		<h4>Exprimez-vous</h4>
+    		<form action="<?php echo RACINE.DS.'messages'.DS.'saveMessage' ?>" method="POST">
+    			<input type="textarea" name="content" id="msgWall"><span class="add-on"><i class="icon-message"></i></span>
+    			<button class="btn btn-primary" id="btn-send">send</button>
+    		</form>
+		  </div>
+    <article id="ressources"> 
+
+      <div class="tabbable"> <!-- Only required for left/right tabs -->
+        <ul class="nav nav-tabs">
+          <li class="active"><a href="#tab1" data-toggle="tab">Messages</a></li>
+          <li><a href="#tab2" data-toggle="tab">Events</a></li>
+          <li><a href="#tab3" data-toggle="tab">Photos</a></li>
+          <li><a href="#tab4" data-toggle="tab">Notifications</a></li>
+        </ul>
+        <div class="tab-content">
+
+          <div class="tab-pane active" id="tab1">
+            <p id="msg-content"></p>
+          </div>
+
+          <div class="tab-pane" id="tab2">
+            <p id="event-content"></p>
+          </div>
+
+          <div class="tab-pane" id="tab3">
+            <p id="photos-content"></p>
+          </div>
+
+          <div class="tab-pane" id="tab4">
+            <p id="notifications-content"></p>
+          </div>
+        
+        </div>
+      </div>
 		
-	</article>
+    </article>
+    
     </div>
     <div class="span2">
       <!--Sidebar content-->
